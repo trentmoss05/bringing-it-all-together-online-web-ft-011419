954e8c2f21a459431fb137a0cdd5da5de8eeb178
Trent Moss <trentmoss05@gmail.com>
2019-02-17 05:14:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,13 @@ class Dog
     end.first
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
